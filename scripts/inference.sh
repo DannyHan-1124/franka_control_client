1a17fe8c2ed6c60977e26f23caa0e42bf45ce54b
DannyHan-1124 <hzd2309@gmail.com>
2026-06-09 12:44:44 +0200

    test full language command

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -13,7 +13,7 @@
 # pgrep -af 'ssh.*(-L)'
 
 python examples/pi05_policy_inference_franka.py \
-    --task "put green cylinder on blue cube" \
+    --task "put red cylinder on yellow cube and put green cylinder on blue cube" \
     --pyzlc_name policy_inference \
     --pyzlc_host 141.3.53.25 \
     --pyzlc_group_name robot_lab_robotiq_202 \
@@ -22,8 +22,6 @@ python examples/pi05_policy_inference_franka.py \
     --policy_zmq_endpoint tcp://127.0.0.1:17725 \
     --chunk_replan_steps 50 \
     --gripper_open_confirm_steps 12 \
-    --stop_after_first_release \
-    --stop_after_release_steps 0 \
     --debug_image_dir debug_images/pi05_inference \
     --debug_image_interval 5 \
     --max_position_step_m 0.0 \
