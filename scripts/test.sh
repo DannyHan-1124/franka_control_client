9eca7f6ea28763005385675e194455f73b2b9899
DannyHan-1124 <hzd2309@gmail.com>
2026-06-12 14:11:07 +0200

    clean up

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,3 @@
-# python -m franka_control_client.policy.pi05_policy_node \
-#     --checkpoint_path /home/jjiang/utphd/pretrained_model \
-#     --dataset_path /home/jjiang/jing/dataset/lerobot/cylinder_full \
-#     --device cuda \
-#     --policy_dtype bfloat16 \
-#     --obs_topic pi05/observation \
-#     --action_topic pi05/action \
-#     --fps 20 \
-#     --default_task "put red cylinder on yellow cube and put green cylinder on blue cube" \
-#     --pyzlc_host 141.3.53.25 \
-#     --pyzlc_group_name robot_lab_robotiq_202 \
-#     --pyzlc_group_port 7725
-
 python -m franka_control_client.policy.pi05_policy_node \
     --checkpoint_path /hkfs/work/workspace/scratch/utphd-myspace/outputs/pi05_bs256_10ksteps_fixed_index/checkpoints/010000/pretrained_model \
     --dataset_path /hkfs/work/workspace/scratch/utphd-myspace/datasets/cylinder_cube_full \
