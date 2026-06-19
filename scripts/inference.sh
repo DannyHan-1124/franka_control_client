ac57b78062fc5795e52896d7d3d866630fe4b361
Jing Jiang <jonasjiang26@outlook.com>
2026-06-19 15:22:25 +0200

    Update logs

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -8,7 +8,7 @@ export PYTHONPATH="${REPO_ROOT}/src:${PYTHONPATH:-}"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put red cylinder on blue cube and put green cylinder on yellow cube" \
+    --task "put green cylinder on blue cube" \
     --stop_after_first_release \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
@@ -18,12 +18,13 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --policy_transport streaming_zmq \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
     --streaming_mode continuous \
-    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics.jsonl" \
-    --chunk_replan_steps 40 \
+    --continuous_min_execute_steps 20 \
+    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_c.jsonl" \
+    --chunk_replan_steps 50 \
     --faster_infer_time_schedule HAS \
     --faster_alpha 1.0 \
     --faster_u0 0.9 \
-    --faster_delay_steps 10 \
+    --faster_delay_steps 0 \
     --phase_fallback_schedule none \
     --static_camera static_cam \
     --wrist_camera wrist_cam
