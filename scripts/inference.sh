9575275fcdf36d83c00a340f64b8c9e50e7a8733
Jing Jiang <jonasjiang26@outlook.com>
2026-06-23 12:43:01 +0200

    Test different parameters

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -8,7 +8,7 @@ export PYTHONPATH="${REPO_ROOT}/src:${PYTHONPATH:-}"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put green cylinder on blue cube" \
+    --task "put red cylinder on red cube" \
     --stop_after_first_release \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
@@ -17,14 +17,14 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --pyzlc_group_port 7725 \
     --policy_transport streaming_zmq \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
-    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_delay_measurement.jsonl" \
-    --execution_horizon 47 \
+    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_overall.jsonl" \
+    --execution_horizon 40 \
     --faster_infer_time_schedule HAS \
     --faster_alpha 1.0 \
     --faster_u0 0.9 \
     --delay 3 \
     --early_stop_actions 0 \
-    --phase_fallback_schedule "none" \
-    --phase_fallback_trigger "before_gripper_open" \
+    --phase_fallback_schedule none \
+    --phase_fallback_trigger before_gripper_open \
     --static_camera static_cam \
     --wrist_camera wrist_cam
