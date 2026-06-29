b8ef269c85c6ecd250362da42754e1e9ac7bca0f
Jing Jiang <jonasjiang26@outlook.com>
2026-06-29 14:45:47 +0200

    change delaz

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -10,7 +10,7 @@ export PYTHONPATH="${REPO_ROOT}/src:${WORKSPACE_ROOT}/lerobot/src"
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --task "put red cylinder on red cube" \
-    --fps 20 \
+    --fps 35 \
     --stop_after_first_release \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
@@ -22,9 +22,9 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_profiling.jsonl" \
     --execution_horizon 25 \
     --faster_infer_time_schedule HAS \
-    --faster_alpha 1.0 \
+    --faster_alpha 0.6 \
     --faster_u0 0.9 \
-    --delay 3 \
+    --delay 4 \
     --early_stop_actions 25 \
     --phase_fallback_schedule none \
     --phase_fallback_trigger before_gripper_open \
