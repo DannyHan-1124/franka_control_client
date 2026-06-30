c038d4883549a119de67d905d51a1deed3c7cc1f
Jing Jiang <jonasjiang26@outlook.com>
2026-06-30 18:30:09 +0200

    debug

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -8,8 +8,7 @@ export PYTHONPATH="${REPO_ROOT}/src:${PYTHONPATH:-}"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put red cylinder on red cube" \
-    --stop_after_first_release \
+    --task "put red cylinder on blue cube and put green cylinder on yellow cube" \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
     --pyzlc_host 141.3.53.25 \
@@ -17,14 +16,18 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --pyzlc_group_port 7725 \
     --policy_transport streaming_zmq \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
-    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_overall.jsonl" \
-    --execution_horizon 40 \
-    --faster_infer_time_schedule HAS \
+    --execution_horizon 50 \
+    --faster_infer_time_schedule const \
     --faster_alpha 1.0 \
     --faster_u0 0.9 \
-    --delay 3 \
+    --delay 0 \
     --early_stop_actions 0 \
     --phase_fallback_schedule none \
     --phase_fallback_trigger before_gripper_open \
     --static_camera static_cam \
     --wrist_camera wrist_cam
+
+# --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_overall.jsonl" \
+# --gripper_open_confirm_steps 12 \
+# --reclose_after_release_min_motion_m 0.08 \
+# --stop_after_first_release \
