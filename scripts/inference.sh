71dadd7ad626b8646aa6e33a69c58e09a9ff86ad
Jing Jiang <jonasjiang26@outlook.com>
2026-07-31 11:49:34 +0200

    new metrics

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -9,7 +9,7 @@ export PYTHONPATH="${REPO_ROOT}/src:${WORKSPACE_ROOT}/lerobot/src"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put cylinder in moving cup" \
+    --task "put red cylinder on red cube" \
     --stop_after_first_release \
     --fps 20 \
     --robot_name FrankaPanda \
@@ -19,12 +19,13 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --pyzlc_group_port 7725 \
     --policy_transport streaming_zmq \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
-    --execution_horizon 50 \
-    --faster_infer_time_schedule const \
+    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_ES.jsonl" \
+    --execution_horizon 15 \
+    --faster_infer_time_schedule HAS \
     --faster_alpha 0.6 \
     --faster_u0 0.9 \
-    --delay 0 \
-    --early_stop_actions 0 \
+    --delay 10 \
+    --early_stop_actions 15 \
     --phase_fallback_schedule none \
     --phase_fallback_trigger before_gripper_open \
     --static_camera static_cam \
