4da9693a16291b51d9cab4d49ea420ae68e8ccc0
Jing Jiang <jonasjiang26@outlook.com>
2026-09-07 23:27:48 +0200

    new metrisc

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -8,7 +8,8 @@ export PYTHONPATH="${REPO_ROOT}/src:${PYTHONPATH:-}"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put cylinder in moving cup" \
+    --task "put red cylinder on red cube" \
+    --fps 20 \
     --stop_after_first_release \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
@@ -16,6 +17,10 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --pyzlc_group_name robot_lab_robotiq_202 \
     --pyzlc_group_port 7725 \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
-    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_dynamic.jsonl" \
+    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_c.jsonl" \
+    --continuous_min_execute_steps 15 \
     --static_camera static_cam \
     --wrist_camera wrist_cam
+
+#   --continuous_min_execute_steps 25 \
+#   --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_dynamic.jsonl" \
