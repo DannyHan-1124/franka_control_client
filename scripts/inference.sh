fd80d7d963d2479f0fea0dcbf971788fabbe24e1
Jing Jiang <jonasjiang26@outlook.com>
2026-09-09 14:38:41 +0200

    new metrics

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -6,16 +6,16 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put red cube in bowl" \
+    --task "put red cylinder on red cube" \
     --stop_after_first_release \
     --pyzlc_name policy_inference \
     --pyzlc_host 141.3.53.25 \
     --pyzlc_group_name robot_lab_robotiq_202 \
     --pyzlc_group_port 7725 \
     --policy_transport zmq \
-    --fps 30 \
-    --chunk_replan_steps 10 \
+    --fps 20 \
     --policy_zmq_endpoint tcp://127.0.0.1:17725 \
+    --metrics_path "${REPO_ROOT}/logs/pi05_puma_inference_metrics_static.jsonl" \
     --static_camera static_cam \
     --wrist_camera wrist_cam
 
