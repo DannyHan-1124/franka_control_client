dd5954f3e737e52ca0c387b0a843a78fc75307d6
Jing Jiang <jonasjiang26@outlook.com>
2026-09-11 14:22:48 +0200

    new metrics

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -9,9 +9,9 @@ export PYTHONPATH="${REPO_ROOT}/src:${WORKSPACE_ROOT}/lerobot/src"
 # pgrep -af 'ssh.*(-L)'
 
 python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
-    --task "put red cylinder on red cube" \
+    --task "put red cube in bowl" \
     --stop_after_first_release \
-    --fps 20 \
+    --fps 30 \
     --robot_name FrankaPanda \
     --pyzlc_name policy_inference \
     --pyzlc_host 141.3.53.25 \
@@ -19,14 +19,14 @@ python "${REPO_ROOT}/examples/pi05_policy_inference_franka.py" \
     --pyzlc_group_port 7725 \
     --policy_transport streaming_zmq \
     --policy_zmq_endpoint tcp://127.0.0.1:17726 \
-    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_original.jsonl" \
-    --first_execution_horizon 20 \
-    --execution_horizon 20 \
-    --faster_infer_time_schedule const \
+    --metrics_path "${REPO_ROOT}/logs/pi05_inference_metrics_test.jsonl" \
+    --first_execution_horizon 30 \
+    --execution_horizon 25 \
+    --faster_infer_time_schedule HAS \
     --faster_alpha 0.6 \
     --faster_u0 0.9 \
-    --delay 0 \
-    --early_stop_actions 0 \
+    --delay 3 \
+    --early_stop_actions 30 \
     --phase_fallback_schedule none \
     --phase_fallback_trigger before_gripper_open \
     --static_camera static_cam \
